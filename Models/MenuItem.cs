53e2cb039f7e34ec0e761bd190a6ca842a077dc9
Thor Richard Hanssen <thor.richard.hanssen@gmail.com>
2019-07-07 20:40:02 +0200

    Setting up left menu with Model

---
+++ Models/MenuItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace DarkFactorCoreNet.Models
+{
+    public class MenuItem
+    {
+        public static string CLASS_MENU = "menu";
+        public static string CLASS_SUBMENU = "submenu";
+        public static string CLASS_DRAFTMENU = "menu_editor";
+        public static string CLASS_DRAFTSUBMENU = "submenu_editor";
+
+        public int ID;
+        public int ParentID;
+        public string Name;
+        public string MenuClass;
+
+        public MenuItem()
+        {
+        }
+    };
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,14 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using DarkFactorCoreNet.Models;
 
 namespace aspnetcoreapp.Pages
 {
     public class IndexModel : PageModel
     {
+        public List<MenuItem> list;
+
         public void OnGet()
         {
-
+            list = new List<MenuItem>();
+            list.Add(new MenuItem() { ID = 1, ParentID = 0, Name = "H O M E", MenuClass = MenuItem.CLASS_MENU });
+            list.Add(new MenuItem() { ID = 2, ParentID = 0, Name = "Codemonkey Blog", MenuClass = MenuItem.CLASS_DRAFTMENU });
+            list.Add(new MenuItem() { ID = 3, ParentID = 2, Name = ".Net Core 2 Website", MenuClass = MenuItem.CLASS_DRAFTSUBMENU });
         }
+
+        public ActionResult OnGetPartial() =>
+
+            new PartialViewResult
+            {
+                ViewName = "Menu",
+                ViewData = ViewData,
+            };
+        
     }
 }
