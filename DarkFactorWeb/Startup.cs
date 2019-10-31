27e14659f455a2be208fb280f4ed27a57b440783
Thor Richard Hanssen <thor.richard.hanssen@gmail.com>
2019-11-01 00:02:58 +0100

    Added Swagger to view APIs

--- DarkFactorWeb/Startup.cs
+++ DarkFactorWeb/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger; 
+using Microsoft.AspNetCore.Http;
 
 using DarkFactorCoreNet.Repository;
 using DarkFactorCoreNet.Repository.Database;
@@ -30,6 +32,12 @@ namespace DarkFactorCoreNet
         {
             services.AddMvc();
 
+            // register the swagger generator
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
+
             services.AddOptions();
             services.AddHttpContextAccessor();
             services.Configure<DatabaseConfig>(Configuration.GetSection("DatabaseConfigModel"));
@@ -41,6 +49,7 @@ namespace DarkFactorCoreNet
             services.AddSingleton(typeof(IMenuRepository), typeof(MenuRepository));
             services.AddSingleton(typeof(IPageRepository), typeof(PageRepository));
             services.AddSingleton(typeof(IDFDatabase), typeof(DFDataBase));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,9 +66,18 @@ namespace DarkFactorCoreNet
 
             app.UseStaticFiles();
             //app.UseHttpsRedirection();
+
+
+            // Enable Swagger middleware 
+            app.UseSwagger(); 
+
+            // specify the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            }); 
+
             app.UseMvc();
-
-
         }
     }
 }
