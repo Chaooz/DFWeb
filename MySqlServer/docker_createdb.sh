7e464f71b47666670e39ad476039c8486b34b0d8
Thor Richard Hanssen <thor.richard.hanssen@gmail.com>
2020-07-07 21:23:28 +0200

    Moved from MySQL => MariaDB

--- MySqlServer/docker_createdb.sh
+++
@@ -1,1 +0,0 @@
-docker exec -i mysqldev mysql -uroot -pnetr00t dfweb < ../../../ExternalTools/Data/darkfactorweb.sql
--- MySqlServer/docker_exportdb.sh
+++
@@ -1,2 +0,0 @@
-docker exec -it darkfactorweb_database_1 mysqldump --complete-insert --extended-insert=FALSE -uroot -psecret dfweb > ./darkfactorweb.sql 
-
