6b69b3220cb74fabb8f4741e529428d3733a5d44
Thor Richard Hanssen <thor.richard.hanssen@gmail.com>
2024-06-10 18:42:18 +0200

    DW-24 Fix for showing articles on pages

--- DarkFactorWeb/Pages/Page.cshtml.cs
+++ DarkFactorWeb/Pages/Page.cshtml.cs
@@ -6,8 +6,6 @@ namespace DarkFactorCoreNet.Pages
 {
     public class Page : FullPageModel
     {
-        public IList<ArticleSectionModel> articleSections;
-
         public Page(IPageProvider pageProvider, IMenuProvider menuProvider, ILoginProvider loginProvider, IImageProvider imageProvider)
          : base(pageProvider,menuProvider, loginProvider, imageProvider)
         {
